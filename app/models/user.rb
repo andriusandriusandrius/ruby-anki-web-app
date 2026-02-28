d8fe1f79517cd0b12b180227fa0f848adf73f5da
andriusandriusandrius <and.zurba@gmail.com>
2026-02-28 15:37:11 +0200

    add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :decks, dependent: :destroy
+
+  has_secure_password
+
+  validates :email, uniqueness: { case_sensitive:false } , presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :password, length: {minimum: 8}, allow_nil: true
 end
