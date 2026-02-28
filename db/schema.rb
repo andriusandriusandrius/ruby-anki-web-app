1d1f115bf40d5dffa65bc06d626b5cb981bc7d16
andriusandriusandrius <and.zurba@gmail.com>
2026-02-28 15:12:57 +0200

    rename to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_27_193854) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_28_131216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_27_193854) do
     t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "name", null: false
-    t.string "password_hash", null: false
+    t.string "password_digest", null: false
     t.datetime "updated_at", null: false
   end
 
